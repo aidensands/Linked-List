a6266dd002628a41e126f3cce9dee8b767e21b5f
24sandsa <88sandsa@gmail.com>
2025-10-03 17:49:02 -0400

    added starting methods and did some testing, not fiully implemented yet

---
+++ src/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Comment
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+    struct Node *previous;
+
+} Node;
+
+Node *new_node(int data){
+    Node *new_node = malloc(sizeof(Node));
+    new_node -> data = data;
+    return new_node;
+}
+
+Node *prepend(Node *head, int v){
+    if (head == NULL){
+        return new_node(v);
+    }
+    else{
+        Node *temp = head;
+        head = new_node(v);
+        head -> next = temp;
+        temp -> previous = head;
+        return head;
+    }
+}
+
+void print_list(Node *head){
+    Node *current = head;
+    while (current != NULL){
+        printf("%d -> ", current -> data);
+        current = current -> next;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    Node *head = NULL;
+    
+    for (int i = 0; i < 10; i++){
+        head = prepend(head, i);
+    }
+
+    print_list(head);
+    return 0;
+}
